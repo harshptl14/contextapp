ea9380c0d0f0c8e35085525ed2da2c1f496c3f22
harshptl14 <harshptl714@gmail.com>
2021-09-30 12:34:11 +0530

    replaced with hooks

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,20 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-export class Booklist extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return (
-      <div className="book-list" style={{background: theme.bg, color: theme.syntax}}>
-        <ul>
-          <li style={{background : theme.ui}}>the way of kings</li>
-          <li style={{background : theme.ui}}>Alchemist</li>
-          <li style={{background : theme.ui}}>the power of a mind</li>
-        </ul>
-      </div>
-    );
-  }
+const Booklist = () => {
+  const {isLightTheme, light, dark} = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div className="book-list" style={{background: theme.bg, color: theme.syntax}}>
+    <ul>
+      <li style={{background : theme.ui}}>the way of kings</li>
+      <li style={{background : theme.ui}}>Alchemist</li>
+      <li style={{background : theme.ui}}>the power of a mind</li>
+    </ul>
+  </div>
+  )
 }
-export default Booklist;
+
+export default Booklist
